a8e0a8d81f4671510d26df49c7f9c62d64b3fa6c
praxhii08 <146984610+praxhii08@users.noreply.github.com>
2025-09-28 14:48:04 +0530

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,10 @@
 
+document.addEventListener('DOMContentLoaded', () => {
+  const links = document.querySelectorAll('.nav-right a');
+  links.forEach(l => {
+    if (l.href === location.href || location.pathname.endsWith(l.getAttribute('href'))) {
+      l.style.color = 'white';
+      l.style.fontWeight = '700';
+    }
+  });
+});
